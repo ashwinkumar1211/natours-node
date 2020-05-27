37946353afc35de7e7425f668ae015d1f5770405
ashish-puttaa <ashishputtaa.98@gmail.com>
2020-05-27 14:05:47 +0530

    add child reference of users in tours

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
+// const User = require('../models/user.model');
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -76,6 +78,32 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    // guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -94,6 +122,13 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// // Embedding guides (users) into tours document
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
 // QUERY MIDDLEWARE
 // Regex - to support find and findOne queries
 tourSchema.pre(/^find/, function (next) {
@@ -102,6 +137,16 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+// Populating the guides fields with user data
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
+// Logging the time taken to preform the query
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} ms`);
   next();
