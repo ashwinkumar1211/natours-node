60d01813d7e0234c952797a7762b8518ba7a20f1
ashish-puttaa <ashishputtaa.98@gmail.com>
2020-05-31 01:55:18 +0530

    add view for the 'User Account Page'

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use('/api', limiter);
 
 // Body Parser - reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 // Cookie Parser
 app.use(cookieParser());
--- controllers/views.controller.js
+++ controllers/views.controller.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tour.model');
+const User = require('../models/user.model');
 const catchAsync = require('../utils/catchAsync.utils');
 const AppError = require('../utils/appError.utils');
 
@@ -32,5 +33,30 @@ exports.getTour = catchAsync(async (req, res, next) => {
 exports.getLoginForm = catchAsync(async (req, res, next) => {
   res.status(200).render('login', {
     title: 'Log into your account',
+  });
+});
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account',
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
   });
 });
